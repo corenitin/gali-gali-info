9de6c602e0da087d7c1e9d3558d50f7d5b91afbf
kirtan0101 <kjpatel200022@gmail.com>
2025-02-22 13:49:25 +0530

    refactor: improve CORS configuration and add comments for clarity

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {connectDB} from "./db/index.js";
+import { connectDB } from "./db/index.js";
 
 config();
 
@@ -12,21 +12,27 @@ const { PORT } = process.env || 8000;
 const ORIGIN = process.env.ORIGIN || "http://localhost:1800";
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/gali-gali-info";
 
-app.use(cors({
-    origin: ORIGIN,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}))
+// CORS Configuration
+app.use(
+  cors({
+    origin: ORIGIN, // Allow requests from this origin
+    credentials: true, // Allow cookies and credentials
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Connect to MongoDB
 connectDB(MONGO_URI);
 
+// Routes
 app.use("/api/users", userRouter);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
