e80be5e5ab421b2815dc199535b6f7431f959b66
kirtan0101 <kjpatel200022@gmail.com>
2025-03-03 00:14:59 +0530

    feat: enhance Dashboard component with loading state management for product categories

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,47 +1,58 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import api from "../api.js";
-
+import { Loading } from "../components";
 
 function Dashboard() {
   const navigate = useNavigate();
-  const [isPending, setIsPending] = useState(false);
-  
+  const [isPendingF, setIsPendingF] = useState(false);
+  const [isPendingG, setIsPendingG] = useState(false);
+
   const [food, setFood] = useState([]);
   const [grocery, setGrocery] = useState([]);
-  
+
   const categories = [
     {
       name: "Food",
       setter: setFood,
-      items: food
+      items: food,
+      pending: isPendingF,
+      setPending: setIsPendingF,
     },
     {
       name: "Grocery",
       setter: setGrocery,
-      items: grocery
+      items: grocery,
+      pending: isPendingG,
+      setPending: setIsPendingG,
     },
   ];
 
-  const fetchProducts = async (category, setCategory) => {
+  const fetchProducts = async (category, setCategory, pending, setPending) => {
+    setPending(true);
     try {
       const res = await api.get(`/business/products/c/${category}`);
       if (res.status === 200) {
         setCategory(res.data.data);
-        console.log(res.data.data)
+        console.log(res.data.data);
       }
     } catch (error) {
       console.log("Error while fetching products ", category);
+    } finally {
+      setPending(false);
     }
   };
 
   useEffect(() => {
-    setIsPending(true);
-    categories.map((item) => (
-      fetchProducts(item.name.toLowerCase(), item.setter)
-    ))
-    setIsPending(false);
-  },[]);
+    categories.map((item) =>
+      fetchProducts(
+        item.name.toLowerCase(),
+        item.setter,
+        item.pending,
+        item.setPending
+      )
+    );
+  }, []);
 
   return (
     <div className="px-6 xs:px-14 sm:px-32 md:px-12 lg:px-44 xl:px-16 py-8 flex flex-col gap-12">
@@ -68,30 +79,40 @@ function Dashboard() {
             <h3 className="head-2 ml-2">{category.name}:</h3>
 
             {/* Product List */}
-            <div className="bg-primary/10 p-4 xs:p-6 rounded-xl flex flex-col md:flex-row justify-evenly items-center">
-              <ul className="grid xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-6">
-                {categories && category.items && category.items.map((item, i) => (
-                  <li
-                    key={i}
-                    className="w-fit bg-light dark:bg-dark rounded-3xl p-4 flex flex-col items-center gap-2 shadow-lg hover:shadow-blue-shadow/25 cursor-pointer"
-                  >
-                    <img
-                      src={item.images[0]}
-                      alt={item.title}
-                      className="w-32 h-32 rounded-2xl"
-                    />
-                    <span className="text-sm xs:text-base">{item.title}</span>
-                  </li>
-                ))}
-              </ul>
+            {category.pending ? (
+              <Loading />
+            ) : (
+              <div className="bg-primary/10 p-4 xs:p-6 rounded-xl">
+                <div className="flex flex-col md:flex-row justify-evenly items-center">
+                  <ul className="grid xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-6">
+                    {categories &&
+                      category.items &&
+                      category.items.map((item, i) => (
+                        <li
+                          key={i}
+                          className="w-fit bg-light dark:bg-dark rounded-3xl p-4 flex flex-col items-center gap-2 shadow-lg hover:shadow-blue-shadow/25 cursor-pointer"
+                        >
+                          <img
+                            src={item.images[0]}
+                            alt={item.title}
+                            className="w-32 h-32 rounded-2xl"
+                          />
+                          <span className="text-sm xs:text-base">
+                            {item.title}
+                          </span>
+                        </li>
+                      ))}
+                  </ul>
 
-              {/* "See More" Button (Adjusts on Mobile) */}
-              <div className="flex justify-center mt-4 xl:mr-4 xl:ml-6">
-                <button className="h-fit text-white bg-primary px-2 py-2 rounded-full hover:bg-primary-dark transition">
-                  See More
-                </button>
+                  {/* "See More" Button (Adjusts on Mobile) */}
+                  <div className="flex justify-center mt-4 xl:mr-4 xl:ml-6">
+                    <button className="h-fit text-white bg-primary px-2 py-2 rounded-full hover:bg-primary-dark transition cursor-pointer">
+                      See More
+                    </button>
+                  </div>
+                </div>
               </div>
-            </div>
+            )}
           </li>
         ))}
       </ul>
