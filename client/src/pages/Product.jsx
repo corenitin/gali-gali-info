e1499de5701645689209c1484997de4747e37623
kirtan0101 <kjpatel200022@gmail.com>
2025-03-10 14:16:45 +0530

    feat: enhance Product page with user details, reviews section, and rating functionality

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -3,14 +3,36 @@ import { useParams } from "react-router";
 import { Loading } from "../components";
 import api from "../api";
 import { MdAlternateEmail } from "react-icons/md";
-import { FaPhoneFlip, FaStar } from "react-icons/fa6";
-import { IoLocationSharp } from "react-icons/io5";
+import { FaPhoneFlip, FaStar, FaRegStar } from "react-icons/fa6";
+import { IoLocationSharp, IoCloseCircle } from "react-icons/io5";
 
 function Product() {
   const { id } = useParams();
   const [isPending, setIsPending] = useState(false);
   const [product, setProduct] = useState(null);
   const [src, setSrc] = useState(null);
+  const [user, setUser] = useState({});
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [rating, setRating] = useState(5);
+  const rate = [1, 2, 3, 4, 5];
+
+  const reviews = [
+    {
+      user: {
+        profileImage:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKOOpLy92UjzQxq8NCxgxOQJbj_YVdfHO_g&s",
+        fullName: "Kirtan Patel",
+      },
+      text: "The taste is so good and restaurant is also very clean with hygiene.   ",
+    },
+    {
+      user: {
+        profileImage: "https://static.toiimg.com/photo/107091667/107091667.jpg",
+        fullName: "Kiton Patel",
+      },
+      text: `The taste is so bad! Don't go for it!`,
+    },
+  ];
 
   const fetchProduct = async () => {
     setIsPending(true);
@@ -22,68 +44,208 @@ function Product() {
         fetchUser(res.data.data.user);
       }
     } catch (error) {
-      console.log("Error while fetching products ", error.response.data.message);
+      console.log(
+        "Error while fetching products ",
+        error.response.data.message
+      );
     } finally {
       setIsPending(false);
     }
   };
 
-  const fetchUser = async(id) => {
+  console.log(product);
+  const fetchUser = async (id) => {
     setIsPending(true);
     try {
-      console.log(id)
+      console.log(id);
       const res = await api.get(`/users/${id}`);
-      console.log(res)
+      console.log(res);
       if (res.status === 200) {
         console.log(res.data.data);
+        setUser(res?.data?.data);
       }
     } catch (error) {
-      console.log("Error while fetching products: ", error.response.data.message);
+      console.log(
+        "Error while fetching products: ",
+        error.response.data.message
+      );
     } finally {
       setIsPending(false);
     }
-  }
+  };
 
   useEffect(() => {
     fetchProduct();
-    
   }, []);
 
   if (isPending) return <Loading />;
 
   return (
-    <div className="p-8 grid grid-cols-2 gap-8">
-      <div>
+    <div>
+      {dialogOpen && (
+        <div className="fixed inset-0 h-[calc(100vh-3.8rem)] w-screen bg-dark/25 dark:bg-light/25 flex justify-center items-center z-50">
+          <div className="relative w-11/12 md:w-96 h-96 bg-light dark:bg-dark rounded-2xl p-4 flex flex-col gap-4">
+            <IoCloseCircle
+              className="absolute right-1 top-1 cursor-pointer hover:text-red-600"
+              size={24}
+              onClick={() => setDialogOpen((prev) => !prev)}
+            />
+            <div className="container-3 flex flex-col items-center bg-dark">
+              <span className="text-sm my-1">Rate the product: </span>
+              <div className="h-px w-full bg-primary rounded-md"></div>
+              <ul className="flex gap-3 items-center my-2">
+                {rate.map((index) => (
+                  <li
+                    key={index}
+                    onClick={() => setRating(index)}
+                    className="flex flex-col items-center p-1 rounded-md bg-primary/5 hover:bg-primary/10 w-12 cursor-pointer border border-transparent hover:border-primary/30 hover:shadow-md transition-all duration-300"
+                  >
+                    {index <= rating ? (
+                      <FaStar className="text-secondary" size={24} />
+                    ) : (
+                      <FaRegStar className="text-secondary" size={24} />
+                    )}
+                    {index}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <textarea
+              className="input h-full w-full"
+              placeholder="Write a review..."
+            />
+          </div>
+        </div>
+      )}
+
+      <div className="p-4 md:p-8 grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
         {product && (
-          <div className="flex gap-8">
+          <div className="flex flex-col xl:flex-row gap-4 w-full">
             <img
               src={src}
               alt={product.title}
-              className="w-96 h-80 rounded-xl object-cover"
+              className="w-full h-72 rounded-xl object-cover"
             />
-            <ul className="grid grid-cols-4 gap-8 container-3">
+            <ul className="grid grid-cols-2 min-[350px]:grid-cols-3 xs:grid-cols-4 sm:grid-cols-6 md:grid-cols-4 xl:grid-cols-3 gap-4 container-3 p-4 w-full justify-items-center">
               {product.images.map((img) => (
                 <img
                   key={img}
                   src={img}
                   alt={product.title}
                   onClick={() => setSrc(img)}
-                  className={`w-20 h-20 rounded-md cursor-pointer ${img === src ? "opacity-100 scale-105" : "opacity-50"} hover:opacity-100 hover:scale-105 transition-all duration-300 object-cover`}
+                  className={`w-20 h-20 rounded-md cursor-pointer ${
+                    img === src ? "opacity-100 scale-105" : "opacity-50"
+                  } hover:opacity-100 hover:scale-105 transition-all duration-300 object-cover`}
                 />
               ))}
             </ul>
           </div>
         )}
-      </div>
-      <div className="container-3">
-        <h2 className="head-2">Contact Details:</h2>
-        <div>
-          <MdAlternateEmail />
-          <span>{}</span>
+
+        <div className="container-3 h-full p-4">
+          <div className="h-full grid xl:grid-rows-1 grid-rows-[auto_auto_1fr] grid-cols-1 xl:grid-cols-[1fr_auto_1fr] gap-4">
+            <div className="pl-2 pt-2 flex flex-col gap-2">
+              <h2 className="head-2">Contact Details:</h2>
+              <div className="flex gap-1 items-center">
+                <MdAlternateEmail className="text-primary" />
+                <span>{user?.email}</span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <FaPhoneFlip className="text-green-500" />
+                <span>{user?.phone}</span>
+              </div>
+              <div className="flex gap-1 items-start">
+                <IoLocationSharp className="text-red-600 mt-1" size={20} />
+                <span>{user?.businessAddress}</span>
+              </div>
+
+              <div className="border border-secondary bg-base-light dark:bg-base-dark p-2 rounded-2xl flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  <img
+                    src={user?.logo}
+                    alt="logo"
+                    className="w-12 h-12 rounded-full"
+                  />
+                  <div className="flex flex-col">
+                    <span className="font-medium">
+                      {user?.organization_name}
+                    </span>
+                    <span className="opacity-50 text-md">
+                      {user?.ownerName}
+                    </span>
+                  </div>
+                </div>
+                <div className="px-2 py-1 bg-primary/20 border border-primary rounded-lg flex items-center gap-2">
+                  <FaStar className="text-secondary" />
+                  <span>{user?.ratings || 0}</span>
+                </div>
+              </div>
+            </div>
+            <div className="bg-secondary w-full xl:w-px h-px xl:h-full rounded-md hidden md:block"></div>
+            <iframe
+              className="h-48 md:h-full w-full bg-lime-400 rounded-lg"
+              loading="lazy"
+
+              allowFullScreen
+              referrerPolicy="no-referrer-when-downgrade"
+              src={`https://www.google.com/maps?q=${user?.businessAddress}&output=embed`}
+            ></iframe>
+          </div>
+        </div>
+
+        <div className="container-3 p-4 space-y-4">
+          <div className="space-y-2">
+            <h2 className="head-2">Description:</h2>
+            <span className="p-2 border border-primary/30 rounded-md bg-primary/10">
+              {product?.desc}
+            </span>
+          </div>
+          <div className="space-y-2">
+            <h2 className="head-2">Offers:</h2>
+            <ul className="px-2 space-y-2">
+              {product?.offers?.map((offer, index) => (
+                <li key={index} className="space-x-1">
+                  <span>{index + 1}.</span>
+                  <span className="p-1 w-fit border border-secondary/30 rounded-md bg-secondary/5">
+                    {offer}.
+                  </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="container-3 p-4">
+          <div className="flex justify-between">
+            <h2 className="head-2">Reviews:</h2>
+            <button
+              className="btn"
+              onClick={() => setDialogOpen((prev) => !prev)}
+            >
+              Add+
+            </button>
+          </div>
+          <ul className="p-2 space-y-4">
+            {reviews.map((review) => (
+              <li
+                key={review.id}
+                className="bg-base-light dark:bg-base-dark flex flex-col border border-secondary dark:border-secondary/75 rounded-2xl"
+              >
+                <div className="flex items-center gap-1 p-2">
+                  <img
+                    src={review.user.profileImage}
+                    alt={`profile_${review.user.fullName}`}
+                    className="h-8 w-8 rounded-full shadow-md object-cover"
+                  />
+                  <span className="font-semibold">{review.user.fullName}</span>
+                </div>
+                <div className="h-px w-full bg-secondary dark:bg-secondary/75"></div>
+                <span className="p-2">{review.text}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-      <div>Details:</div>
-      <div>Reviews:</div>
     </div>
   );
 }
